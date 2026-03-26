74f9ee3db1911e63a468a7a4aa173bda249be141
JW = UEY HUA COX(COW) <170684406+Joshuawcox5@users.noreply.github.com>
2026-03-25 20:26:47 -0400

    Update content in joshuaDom.js for clarity

--- joshuaSpace/joshuaDom.js
+++ joshuaSpace/joshuaDom.js
@@ -1,8 +1,8 @@
 DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-warning">
         <h1>JOSHUA SPACE</h1>
-        <p>This where you will work</p>
-        <h2>TRY BRANCH</h2>
-        <p>Joshua Samonte</p>
+        <p>This where you will tag different assets</p>
+        <h2>TRY new direction and start a branch here</h2>
+        <p>Joshua Wade Cox (depending on where you all spin around center)</p>
     </div>
     `);
